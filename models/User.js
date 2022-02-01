eb4eace0630eb6fc7f9b082e3cbe06c9156fe28f
khanhlam90 <91096296+khanhlam90@users.noreply.github.com>
2022-01-31 22:45:45 -0800

    fix bugs on user model

--- models/User.js
+++ models/User.js
@@ -26,14 +26,6 @@ User.init(
         allowNull: false,
         unique: true
     },
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true, 
-        validate: {
-          isEmail: true 
-        }
-    },
     password: {
         type: DataTypes.STRING,
         allowNull: false,
