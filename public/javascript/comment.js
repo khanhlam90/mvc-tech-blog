ca8372dd4b851e652dd0820838492ef0d5359ff2
khanhlam90 <91096296+khanhlam90@users.noreply.github.com>
2022-02-01 01:19:35 -0800

    fix bug for comment submit button

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -1,7 +1,7 @@
 async function commentFormHandler(event) {
     event.preventDefault();
   
-    const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
+    const comment_text = document.querySelector('input[name="comment-body"]').value.trim();
   
     const post_id = window.location.toString().split('/')[
       window.location.toString().split('/').length - 1
@@ -28,4 +28,4 @@ async function commentFormHandler(event) {
       }
   }
   
-document.querySelector('#comment-form').addEventListener('submit', commentFormHandler);
+document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
